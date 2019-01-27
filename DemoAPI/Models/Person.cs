74608306d48974e118b127ddfb56d0773ba29852
Allan Camilo <acamilo.ag@gmail.com>
2019-01-27 16:23:29 +0200

    Person Model
    Added a Person Model class to help in the demo operations. later was added the XML documentation to be showing the default documentation.

---
+++ DemoAPI/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Models
+{
+    /// <summary>
+    /// Represent a single user
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// User unique identifier.
+        /// </summary>
+        public int Id { get; set; } = 0;
+        /// <summary>
+        /// The user First Name
+        /// </summary>
+        public string FirstName { get; set; } = "";
+        /// <summary>
+        /// The user Last Name
+        /// </summary>
+        public string LastName { get; set; } = "";
+    }
+}
